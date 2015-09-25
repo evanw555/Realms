87fe3266ca78533c83aa5b18614d4c62ad1545d4
Evan Williams <evanw555@gmail.com>
2015-09-24 17:41:48 -0700

    Base URL of game app now blank.

--- wsgi/myproject/game/views.py
+++ wsgi/myproject/game/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.http import HttpResponse, HttpRequest, HttpResponseRedirect
+from django.shortcuts.urlresolvers import reverse
 
 # Create your views here.
 
 
 def helloworld(request):
     return HttpResponse('Hello, world')
+
--- wsgi/myproject/myproject/urls.py
+++ wsgi/myproject/myproject/urls.py
@@ -2,9 +2,9 @@
 
 from django.conf.urls import include, url
 from django.contrib import admin
+from ..game import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^game/', include('game.urls')),
-    url(r'^$', include('game.urls')),
+    url(r'^', include('game.urls')),
 ]
