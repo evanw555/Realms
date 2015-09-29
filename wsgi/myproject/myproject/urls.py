9c178e9d5f4943915ea1cd2d0d1971fb952b5fb8
Evan Williams <evanw555@gmail.com>
2015-09-28 17:33:21 -0700

    Added favicon, JQuery testing.

--- wsgi/myproject/myproject/urls.py
+++ wsgi/myproject/myproject/urls.py
@@ -1,9 +1,11 @@
 """myproject URL Configuration"""
 
+from django.views.generic.base import RedirectView
 from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^favicon\.ico$', RedirectView.as_view(url='/static/favicon.ico')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('game.urls')),
 ]
