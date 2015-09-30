00c31250a458963672f2d1d06fc904c906175321
Evan Williams <evanw555@gmail.com>
2015-09-30 00:34:12 -0700

    Added JQuery slider UI to homepage, minor URL/formatting fixes.

--- wsgi/myproject/game/urls.py
+++ wsgi/myproject/game/urls.py
@@ -4,8 +4,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from . import views
 
-urlpatterns = [
-    url(r'^$', views.homepage),
-    url(r'^test$', views.homepage, name='home'),
+urlpatterns = (
+    url(r'^$', views.homepage, name='home'),
+    url(r'^test$', views.homepage),
     url(r'^about$', views.about, name='about'),
-]
+)
