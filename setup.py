487b2a934be53431f03ee1f6f45507da2b3ba58f
Evan Williams <evanw555@gmail.com>
2015-10-04 23:46:20 -0700

    Fix.

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ from setuptools import setup
 
 setup(
     # GETTING-STARTED: set your app name:
-    name='YourAppName',
+    name='realms',
     # GETTING-STARTED: set your app version:
     version='1.0',
     # GETTING-STARTED: set your app description:
-    description='OpenShift App',
+    description='Realms Game',
     # GETTING-STARTED: set author name (your name):
-    author='Your Name',
+    author='Evan Williams',
     # GETTING-STARTED: set author email (your email):
-    author_email='example@example.com',
+    author_email='evanw555@gmail.com',
     # GETTING-STARTED: set author url (your url):
-    url='http://www.python.org/sigs/distutils-sig/',
+    url='https://github.com/evanw555',
     # GETTING-STARTED: define required django version:
     install_requires=[
         'Django==1.8.4'
