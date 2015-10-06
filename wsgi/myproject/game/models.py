8d3644ea972142b533e36bbd9af180ff7298c136
Evan Williams <evanw555@gmail.com>
2015-10-06 14:13:19 -0700

    Fix.

--- wsgi/myproject/game/models.py
+++ wsgi/myproject/game/models.py
@@ -39,38 +39,38 @@ class Realm(models.Model):
     def __str__(self):
         return self.name
 
-    def __getitem__(self, item):
-        try:
-            return self.zone_set.filter(row=item)
-        except:
-            return None
-
-    def get_zone(self, r, c):
-        try:
-            return self.zone_set.get(row=r, column=c)
-        except:
-            return None
-
-    class Meta:
-        managed = True
-        app_label = 'game'
-
-
-class Zone(models.Model):
-    realm = models.ForeignKey(Realm)
-    row = models.PositiveSmallIntegerField(default=0)
-    column = models.PositiveSmallIntegerField(default=0)
-    type = models.PositiveSmallIntegerField(default=0)
-
-    def __str__(self):
-        return '<{}@{},{}: {}>'.format(str(self.realm), str(self.row), str(self.column), self.str_type())
-
-    def str_type(self):
-        return {
-            0: 'sea',
-            1: 'plains',
-        }[int(self.type)]
+    # def __getitem__(self, item):
+    #     try:
+    #         return self.zone_set.filter(row=item)
+    #     except:
+    #         return None
+    #
+    # def get_zone(self, r, c):
+    #     try:
+    #         return self.zone_set.get(row=r, column=c)
+    #     except:
+    #         return None
 
     class Meta:
         managed = True
         app_label = 'game'
+
+
+# class Zone(models.Model):
+#     realm = models.ForeignKey(Realm)
+#     row = models.PositiveSmallIntegerField(default=0)
+#     column = models.PositiveSmallIntegerField(default=0)
+#     type = models.PositiveSmallIntegerField(default=0)
+#
+#     def __str__(self):
+#         return '<{}@{},{}: {}>'.format(str(self.realm), str(self.row), str(self.column), self.str_type())
+#
+#     def str_type(self):
+#         return {
+#             0: 'sea',
+#             1: 'plains',
+#         }[int(self.type)]
+#
+#     class Meta:
+#         managed = True
+#         app_label = 'game'
