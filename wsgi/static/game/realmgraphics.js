c700a9eb3fc89f4c568ac80867b6381dd6b0f9f3
Evan Williams <evanw555@gmail.com>
2015-10-07 22:10:40 -0700

    Migrated Realm page JS to separate file, added hover box for tiles.

---
+++ wsgi/static/game/realmgraphics.js
@@ -0,0 +1,44 @@
+
+var myPanel, hoverBox, zoneTypes = [];
+
+
+function load_context_data(_zoneTypes) {
+    zoneTypes = _zoneTypes;
+}
+
+$(document).ready(function() {
+    try{
+        myPanel = new jsgl.Panel(document.getElementById("canvas"));
+        hoverBox = myPanel.createRectangle();
+        hoverBox.setHeight(32);
+        hoverBox.setWidth(32);
+        hoverBox.getFill().setOpacity(0.0);
+        hoverBox.getStroke().setWeight(3);
+        myPanel.addElement(hoverBox);
+
+          /* Create circle and modify it */
+        for(var r = 0; r < zoneTypes.length; r++)
+            for(var c = 0; c < zoneTypes[r].length; c++){
+                var tile = myPanel.createRectangle();
+                tile.setLocationXY(32*c, 32*r);
+                tile.setHeight(32);
+                tile.setWidth(32);
+                tile.getStroke().setWeight(0);
+                tile.getFill().setColor("rgb(16,"+(zoneTypes[r][c]==1?255:0)+
+                                            ","+(zoneTypes[r][c]==0?255:0)+")");
+                tile.addMouseOverListener(function() {
+                    hoverBox.setLocationXY(32*c, 32*r);
+                });
+                myPanel.addElement(tile);
+            }
+        var label = myPanel.createLabel();
+        label.setText("Welcome to this Realm!");
+        label.setLocationXY(Math.floor(Math.random() * 400), Math.floor(Math.random() * 400));
+        label.setBold(true);
+        label.setFontFamily("sans-serif");
+        label.setFontSize(24);
+        myPanel.addElement(label);
+    }catch(err){
+        console.log("JSGL error");
+    }
+});
