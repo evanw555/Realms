70e556f1125fef5b14a44ec12d3b321df33bc9b9
Evan Williams <evanw555@gmail.com>
2015-10-11 16:17:00 -0700

    tree and mountain zones, new generation algorithm.

--- wsgi/myproject/game/maputil.py
+++ wsgi/myproject/game/maputil.py
@@ -15,17 +15,49 @@ def generate_random(realm):
     # if realm already has zones, delete them
     if realm.zone_set.all().count() != 0:
         realm.zone_set.all().delete()
-    # randomly generate realm
+    # make tenative grid of zone types
+    map = []
+    for r in range(REALM_HEIGHT):
+        row = []
+        for c in range(REALM_WIDTH):
+            row.append(0)
+        map.append(row)
+    # randomly generate water and land
     for r in range(REALM_HEIGHT):
         for c in range(REALM_WIDTH):
             if r == 0 or c == 0:
-                realm.zone_set.create(row=r, column=c, type=random.randint(0, 1))
+                map[r][c] = random.randint(0, 1)
             else:
-                realm.zone_set.create(row=r, column=c,
-                                      type=random.choice([
-                                          realm.get_zone(r-1, c).type,
-                                          realm.get_zone(r, c-1).type,
-                                          realm.get_zone(r-1, c-1).type,
-                                          0, 1, 1,
-                                      ]))
+                map[r][c] = random.choice([
+                    map[r-1][c],
+                    map[r][c-1],
+                    map[r-1][c-1],
+                    0, 1, 1,
+                ])
+    # randomly generate forests and mountains
+
+    def recursive_set(row, column, type, chance, reduction):
+        if row < 0 or row >= REALM_HEIGHT or column < 0 or column >= REALM_WIDTH:
+            return
+        if random.randint(0, 100) <= chance:
+            map[row][column] = type
+            recursive_set(row-1, column, type, chance // reduction, reduction)
+            recursive_set(row, column-1, type, chance // reduction, reduction)
+            recursive_set(row+1, column, type, chance // reduction, reduction)
+            recursive_set(row, column+1, type, chance // reduction, reduction)
+
+    r, c = random.randint(0, REALM_HEIGHT-1), random.randint(0, REALM_WIDTH-1)
+    while map[r][c] != 0:
+        r, c = random.randint(0, REALM_HEIGHT-1), random.randint(0, REALM_WIDTH-1)
+    recursive_set(r, c, 2, 100, 1.6)
+
+    r, c = random.randint(0, REALM_HEIGHT-1), random.randint(0, REALM_WIDTH-1)
+    while map[r][c] != 0:
+        r, c = random.randint(0, REALM_HEIGHT-1), random.randint(0, REALM_WIDTH-1)
+    recursive_set(r, c, 3, 100, 2.0)
+
+    # save map to realm model
+    for r in range(REALM_HEIGHT):
+        for c in range(REALM_WIDTH):
+            realm.zone_set.create(row=r, column=c, type=map[r][c])
     realm.save()
